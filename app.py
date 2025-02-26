3a5fff05cc01970ee911a2e32155a2443efc7f87
akapoor810 <anika.kapoor810@gmail.com>
2025-02-26 18:17:08 -0500

    increased k back

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ def main():
         system=sys_instructions,
         query=message,
         temperature=0.0,
-        lastk=0,
+        lastk=10,
         session_id='comp150-cdr-2025s-Ic636oMxYQJviNamr6P6DAmWO45leqi3ZRcBLrl2',
         rag_usage=True,
         rag_threshold='0.3',
